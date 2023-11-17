efbef6d99d7e92d75b58f155022ad8b85c909751
smzalam <alam.syedmz@gmail.com>
2023-11-17 10:34:51 -0500

    initialized appwrite

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,21 @@
+import { Client, Account, Databases, Storage, Avatars } from 'appwrite';
+
+export const appwriteConfig = {
+    url: import.meta.env.VITE_APPWRITE_URL,
+    projectID: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    databaseID: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+    storageID: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    usersCollectionID: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+    postsCollectionID: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
+    savesCollectionID: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+}
+
+export const client  = new Client();
+
+client.setProject(appwriteConfig.projectID);
+client.setEndpoint(appwriteConfig.url);
+
+export const account  = new Account(client);
+export const databases  = new Databases(client);
+export const storage  = new Storage(client);
+export const avatars  = new Avatars(client);
